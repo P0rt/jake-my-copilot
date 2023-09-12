ca03012bdbd15ab8a9f7f1a6701b0e8f9b1675ff
Sergei Parfenov <serzhooo@gmail.com>
2023-09-12 08:13:34 +0200

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,21 +1,36 @@
 from flask import Flask, request, jsonify
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 class Predict(Resource):
+    """
+    Resource to handle predictions.
+    """
+    
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('code', type=str, required=True,
+                                   help='No code data provided', location='json')
+    
     def post(self):
+        """
+        Accepts JSON input with a 'code' key, and returns a prediction.
+        """
         
-        data = request.get_json(force=True)  
-        code_input = data.get("code", "")
+        # Parse and validate incoming data
+        try:
+            args = self.reqparse.parse_args()
+            code_input = args['code']
 
-        # This for example CodeLlama
-        # Example:
-        # output = model.predict(code_input)
-        output = f"Done: {code_input}. Here your data."
+            # This simulates using a model to make predictions on the code_input
+            # Ideally, you'd call your model here.
+            output = f"Done: {code_input}. Here's your data."
 
-        return jsonify({"prediction": output})
+            return jsonify({"prediction": output}), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
 
 api.add_resource(Predict, "/predict")
 
